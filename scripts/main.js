1065fd75d9df4640a5e5ac36581fb87240ff622a
Lilli Freischem <lillifreischem@lillismacbook.local>
2019-02-09 16:28:32 +0000

    fixed resume js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 const RESUME = document.querySelector(".resume");
 
 function reveal(e) {
-  e.preventDefailt();
-  RESUME.innerHTML = "Baaaaa";
+  if (RESUME.innerHTML === "Resume") {
+    RESUME.innerHTML = "B a a a a";
+  } else {
+    RESUME.innerHTML = "Resume";
+  }
 }
 
 RESUME.addEventListener("click", reveal, false);
