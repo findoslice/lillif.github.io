a3d7c817e7b8f4078ddc7e5fe91aeb06bc739c00
Lilli Freischem <lillifreischem@c118191.wlan.net.ed.ac.uk>
2019-02-12 15:37:22 +0000

    added version1 of collapsed nav when window gets small

--- scripts/main.js
+++ scripts/main.js
@@ -18,17 +18,33 @@ window.addEventListener("resize", navAdjust, false);
 function navAdjust() {
   /* if less than 700px wide, collapse */
   let myNav = document.getElementById("mynav");
+  let navButton = document.getElementById("mynavbutton");
+  let myList = document.getElementById("mynavlist");
+  let myLinks = document.getElementsByClassName("nav_link");
   if (window.innerWidth < 700) {
     /* collapse */
     /* if nav bar contains class small don't do anything, otherwise add */
-    if (!myNav.classList.contains("small")) {
-      myNav.classList.add("small");
+    if (!myNav.classList.contains("collapsed_nav")) {
+      myNav.classList.add("collapsed_nav");
+      myList.classList.add("collapsed_list");
+      for (let item of myLinks) {
+        console.log(item.classList);
+        item.classList.add("collapsed_link");
+      }
+      navButton.classList.remove("hide");
       console.log("added small");
     }
   } else {
     /* expand */
-    if (myNav.classList.contains("small")) {
-      myNav.classList.remove("small");
+    if (myNav.classList.contains("collapsed_nav")) {
+      myNav.classList.remove("collapsed_nav");
+
+      myList.classList.remove("collapsed_list");
+
+      for (let item of myLinks) {
+        item.classList.remove("collapsed_link");
+      }
+      navButton.classList.add("hide");
       console.log("removed small");
     }
   }
