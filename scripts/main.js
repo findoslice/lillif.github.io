dc3606897e6f72394924b1e1040456310c2218e3
Lilli Freischem <lillifreischem@c118021.wlan.net.ed.ac.uk>
2019-02-12 11:57:21 +0000

    made nav an ol

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,27 @@ function reveal(e) {
 }
 
 RESUME.addEventListener("click", reveal, false);
+
+/* add window event listener,
+if window gets smaller than X, collapse nav bar */
+
+window.addEventListener("resize", navAdjust, false);
+
+function navAdjust() {
+  /* if less than 700px wide, collapse */
+  let myNav = document.getElementById("mynav");
+  if (window.innerWidth < 700) {
+    /* collapse */
+    /* if nav bar contains class small don't do anything, otherwise add */
+    if (!myNav.classList.contains("small")) {
+      myNav.classList.add("small");
+      console.log("added small");
+    }
+  } else {
+    /* expand */
+    if (myNav.classList.contains("small")) {
+      myNav.classList.remove("small");
+      console.log("removed small");
+    }
+  }
+}
